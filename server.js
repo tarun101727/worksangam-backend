7d3895b3da738fc3d0105626a720d0b38d19f341
tarun101727 <gaddamtarun157@gmail.com>
2026-03-20 11:54:24 +0530

    backend

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ initSocket(server);
 
 const allowedOrigins = [
   "https://www.worksangam.in",
-  "https://www.worksangam.in/signup",
+  "https://worksangam-backend.onrender.com.",
   "https://worksangam.in"
 ];
 
