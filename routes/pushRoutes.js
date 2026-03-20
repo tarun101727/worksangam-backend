331e1bd27c609f1e0ed9154a18581948385a7f7a
tarun101727 <gaddamtarun157@gmail.com>
2026-03-20 14:38:45 +0530

    backend

--- routes/pushRoutes.js
+++ routes/pushRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
+import webpush from "../utils/push.js"; // your configured web-push
 
 const router = express.Router();
-
 let subscriptions = [];
 
+// Endpoint to get public key
+router.get("/public-key", (req, res) => {
+  res.json({ publicKey: process.env.VAPID_PUBLIC_KEY });
+});
+
+// Subscribe endpoint
 router.post("/subscribe", (req, res) => {
   const sub = req.body;
   subscriptions.push(sub);
