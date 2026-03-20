d5dbced71c19930fb9658db07b7662158187ea80
tarun101727 <gaddamtarun157@gmail.com>
2026-03-20 14:36:54 +0530

    backend

---
+++ generateVapid.cjs
@@ -0,0 +1,7 @@
+// backend/generateVapid.cjs
+const webpush = require("web-push");
+
+const vapidKeys = webpush.generateVAPIDKeys();
+
+console.log("Public Key:", vapidKeys.publicKey);
+console.log("Private Key:", vapidKeys.privateKey);
