9e8ed36f40e284a648e8bd0b2dc13cbcdb06a0d8
tarun101727 <gaddamtarun157@gmail.com>
2026-03-22 17:17:42 +0530

    backend

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 import postmark from "postmark";
-import User from '../models/User.js'; 
+import User from '../models/User.js';  
 import bcrypt from 'bcryptjs'; 
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv'; 
