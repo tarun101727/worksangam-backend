40a220414eafa2d2da1a330ebcd38a419d91b467
tarun101727 <gaddamtarun157@gmail.com>
2026-03-22 15:07:46 +0530

    backend

---
+++ middleware/uploadCloudinary.js
@@ -0,0 +1,15 @@
+// backend/middleware/uploadCloudinary.js
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "avatars",       // folder in Cloudinary
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
+    transformation: [{ width: 500, height: 500, crop: "limit" }],
+  },
+});
+
+export const uploadAvatarCloud = multer({ storage });
