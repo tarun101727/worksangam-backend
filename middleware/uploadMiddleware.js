880eac546c03aae5ebe4386ad2a840f1fe7dda37
tarun101727 <gaddamtarun157@gmail.com>
2026-03-27 12:54:23 +0530

    Update uploadMiddleware.js

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,15 +1,6 @@
 import multer from "multer";
-import path from "path";
 
-const storage = multer.diskStorage({
-  destination: "uploads/hirer-posts",
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      `${Date.now()}-${Math.round(Math.random() * 1e9)}${path.extname(file.originalname)}`
-    );
-  },
-});
+const storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
   if (
