08aaa72bdcbb0828719b4178681a5360e17f152e
tarun101727 <gaddamtarun157@gmail.com>
2026-04-05 20:23:22 +0530

    Update translate.js

--- utils/translate.js
+++ utils/translate.js
@@ -1,13 +1,22 @@
-import pkg from "@google-cloud/translate";
-const { v2 } = pkg;
+import { TranslationServiceClient } from "@google-cloud/translate";
 
-const translate = new v2.Translate({
-  key: process.env.GOOGLE_API_KEY,
+const client = new TranslationServiceClient({
+  key: process.env.GOOGLE_API_KEY
 });
 
-export const translateText = async (text, target = "te") => {
+export const transliterateText = async (text, targetLanguage = "te") => {
   if (!text) return "";
 
-  const [translation] = await translate.translate(text, target);
-  return translation;
+  const request = {
+    parent: `projects/${process.env.GOOGLE_PROJECT_ID}/locations/global`,
+    contents: [text],
+    mimeType: "text/plain",
+    sourceLanguageCode: "en",  // Latin input
+    targetLanguageCode: targetLanguage, // Telugu
+  };
+
+  const [response] = await client.transliterateText(request);
+
+  // Returns transliterated text
+  return response.translations.map(t => t.translatedText).join(" ");
 };
