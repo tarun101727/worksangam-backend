b0755b26ea008a257e8cbefa58ae591afe1ded33
tarun101727 <gaddamtarun157@gmail.com>
2026-04-05 21:00:57 +0530

    Update translate.js

--- utils/translate.js
+++ utils/translate.js
@@ -1,22 +1,13 @@
-import { TranslationServiceClient } from "@google-cloud/translate";
+import pkg from "@google-cloud/translate";
+const { v2 } = pkg;
 
-const client = new TranslationServiceClient({ key: process.env.GOOGLE_API_KEY });
+const translate = new v2.Translate({
+  key: process.env.GOOGLE_API_KEY,
+});
 
-export const transliterateText = async (text, targetLanguageCode = "te") => {
+export const translateText = async (text, target = "te") => {
   if (!text) return "";
 
-  const request = {
-    parent: `projects/${process.env.GOOGLE_PROJECT_ID}/locations/global`,
-    contents: [text],
-    mimeType: "text/plain",
-    sourceLanguageCode: "en",
-    targetLanguageCode,
-    model: "nmt",
-  };
-
-  const [response] = await client.translateText(request);
-
-  // Transliteration comes from `translations[0].transliteratedText`
-  const transliterated = response.translations[0]?.transliteratedText || "";
-  return transliterated;
+  const [translation] = await translate.translate(text, target);
+  return translation;
 };
