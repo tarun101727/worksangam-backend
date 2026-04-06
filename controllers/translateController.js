de59e4631d97b764a4a29e02ec8a881131e86301
tarun101727 <gaddamtarun157@gmail.com>
2026-04-06 23:59:21 +0530

    Add files via upload

---
+++ controllers/translateController.js
@@ -0,0 +1,48 @@
+// controllers/translateController.js
+import Translate from "../models/Translate.js";
+
+/* Upload / Add translations */
+export const addLanguageTranslations = async (req, res) => {
+  try {
+    const { languageCode, languageName, translations } = req.body;
+
+    if (!languageCode || !languageName || !translations) {
+      return res.status(400).json({ msg: "All fields are required" });
+    }
+
+    const existing = await Translate.findOne({ languageCode });
+
+    if (existing) {
+      existing.translations = translations;
+      existing.languageName = languageName;
+      await existing.save();
+      return res.json({ msg: "Translations updated", language: existing });
+    }
+
+    const newLang = new Translate({ languageCode, languageName, translations });
+    await newLang.save();
+
+    res.json({ msg: "Translations added", language: newLang });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
+/* Get translations by language code */
+export const getTranslations = async (req, res) => {
+  try {
+    const { lang } = req.params;
+
+    const translations = await Translate.findOne({ languageCode: lang });
+
+    if (!translations) {
+      return res.status(404).json({ msg: "Language not found" });
+    }
+
+    res.json(translations.translations);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Server error" });
+  }
+};
