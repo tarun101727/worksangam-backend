a0089fe8f93c479782ba4aa2e14382bf01d97500
tarun101727 <gaddamtarun157@gmail.com>
2026-04-17 16:13:13 +0530

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -34,26 +34,31 @@ export const createOrder = async (req, res) => {
     });
 
     const response = await axios.post(
-      "https://sandbox.cashfree.com/pg/orders",
-      {
-        order_id: orderId,
-        order_amount: amount,
-        order_currency: "INR",
-        customer_details: {
-          customer_id: userId,
-          customer_email: user.email, // ✅ FIXED
-            customer_phone: "9" + Math.floor(100000000 + Math.random() * 900000000),
+  "https://api.cashfree.com/pg/orders",
+  {
+    order_id: orderId,
+    order_amount: amount,
+    order_currency: "INR",
 
-        },
-      },
-      {
-        headers: {
-          "x-client-id": process.env.CASHFREE_APP_ID,
-          "x-client-secret": process.env.CASHFREE_SECRET_KEY,
-          "x-api-version": "2022-09-01",
-        },
-      }
-    );
+    customer_details: {
+      customer_id: userId,
+      customer_email: user.email,
+      customer_phone:
+        "9" + Math.floor(100000000 + Math.random() * 900000000),
+    },
+
+    order_meta: {
+      return_url: `https://worksangam.in/payment-success?order_id=${orderId}`,
+    },
+  },
+  {
+    headers: {
+      "x-client-id": process.env.CASHFREE_APP_ID,
+      "x-client-secret": process.env.CASHFREE_SECRET_KEY,
+      "x-api-version": "2022-09-01",
+    },
+  }
+);
 
     res.json({
       payment_session_id: response.data.payment_session_id,
