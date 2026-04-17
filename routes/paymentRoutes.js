0c2edcedc83789e16414e5c2271d3cfbab1e5db2
tarun101727 <gaddamtarun157@gmail.com>
2026-04-17 12:36:14 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,9 +1,11 @@
+// routes/paymentRoutes.js
 import express from "express";
+import { createOrder, cashfreeWebhook } from "../controllers/paymentController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
-import { addCredits } from "../controllers/paymentController.js";
 
 const router = express.Router();
 
-router.post("/add-credits", authMiddleware, addCredits);
+router.post("/create-order", authMiddleware, createOrder);
+router.post("/webhook", cashfreeWebhook);
 
 export default router;
