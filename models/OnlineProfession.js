639c029fa08ca413e188eacb268d7b48ece522ce
tarun101727 <gaddamtarun157@gmail.com>
2026-04-25 20:12:48 +0530

    Update OnlineProfession.js

--- models/OnlineProfession.js
+++ models/OnlineProfession.js
@@ -8,12 +8,31 @@ const OnlineProfessionSchema = new mongoose.Schema({
     trim: true
   },
 
-  translations: {
-    te: { type: String }, // Telugu
-    hi: { type: String }, // Hindi (optional)
-    ta: { type: String },
-    kn: { type: String }
-  }
+ translations: {
+  en: { type: String },
+  te: { type: String },
+  hi: { type: String },
+  as: { type: String },
+  bn: { type: String },
+  brx: { type: String },
+  doi: { type: String },
+  gu: { type: String },
+  kn: { type: String },
+  ks: { type: String },
+  kok: { type: String },
+  mai: { type: String },
+  ml: { type: String },
+  mni: { type: String },
+  mr: { type: String },
+  ne: { type: String },
+  or: { type: String },
+  pa: { type: String },
+  sa: { type: String },
+  sat: { type: String },
+  sd: { type: String },
+  ta: { type: String },
+  ur: { type: String }
+}
 });
 
 export default mongoose.model("OnlineProfession", OnlineProfessionSchema);
