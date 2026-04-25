7875354085f4d3a5289e6eb20463c8039525ba30
tarun101727 <gaddamtarun157@gmail.com>
2026-04-25 20:13:04 +0530

    Update OfflineProfession.js

--- models/OfflineProfession.js
+++ models/OfflineProfession.js
@@ -9,11 +9,30 @@ const OfflineProfessionSchema = new mongoose.Schema({
   },
 
   translations: {
-    te: { type: String }, // Telugu
-    hi: { type: String }, // Hindi
-    ta: { type: String }, // Tamil
-    kn: { type: String }, // Kannada
-  }
+  en: { type: String },
+  te: { type: String },
+  hi: { type: String },
+  as: { type: String },
+  bn: { type: String },
+  brx: { type: String },
+  doi: { type: String },
+  gu: { type: String },
+  kn: { type: String },
+  ks: { type: String },
+  kok: { type: String },
+  mai: { type: String },
+  ml: { type: String },
+  mni: { type: String },
+  mr: { type: String },
+  ne: { type: String },
+  or: { type: String },
+  pa: { type: String },
+  sa: { type: String },
+  sat: { type: String },
+  sd: { type: String },
+  ta: { type: String },
+  ur: { type: String }
+}
 });
 
 export default mongoose.model("OfflineProfession", OfflineProfessionSchema);
