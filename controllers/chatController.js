6e85af078e5c3bdc90aa98a7d10651b3dd6cce8b
tarun101727 <gaddamtarun157@gmail.com>
2026-05-01 21:26:12 +0530

    Update chatController.js

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,4 +1,4 @@
-import cloudinary from "./config/cloudinary.js";
+import cloudinary from "../config/cloudinary.js";
 import fs from "fs";
 import Chat from "../models/Chat.js";
 import Message from "../models/Message.js";
