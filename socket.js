1ff4d12410c5afc85df25464bf3bc68521097f0e
tarun101727 <gaddamtarun157@gmail.com>
2026-05-22 00:02:54 +0530

    Update socket.js

--- socket.js
+++ socket.js
@@ -4,11 +4,13 @@ let io;
 
 export const initSocket = (server) => {
   io = new Server(server, {
-    cors: {
-      origin: true,
-      credentials: true,
-    },
-  });
+  transports: ["websocket"],
+
+  cors: {
+    origin: true,
+    credentials: true,
+  },
+});
 
   io.on("connection", (socket) => {
     const userId = socket.handshake.auth.userId;
@@ -54,24 +56,6 @@ socket.on("typing", ({ chatId, userId }) => {
 
 socket.on("stop-typing", ({ chatId, userId }) => {
   socket.to(chatId).emit("user-stop-typing", { userId });
-});
-
-   socket.on("send-message", ({ chatId, message, sender, receiverId }) => {
-
-  // send message to chat room
-  socket.to(chatId).emit("receive-message", {
-    message,
-    sender
-  });
-
-  // 🔥 send notification to receiver directly
-  io.to(receiverId).emit("new-chat-notification", {
-    message,
-    sender,
-    chat: chatId,
-    createdAt: new Date()
-  });
-
 });
 
     /* -------------------- USER ROOM -------------------- */
