bdd2ed84c08dc0b9cb2a387a35b272f1a7f82855
tarun101727 <gaddamtarun157@gmail.com>
2026-06-06 18:13:52 +0530

    Update Payment.js

--- models/Payment.js
+++ models/Payment.js
@@ -1,17 +1,31 @@
-// models/Payment.js
 import mongoose from "mongoose";
 
 const paymentSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+
   orderId: String,
+
   amount: Number,
-  credits: Number,
+
+  plan: {
+    type: String,
+    enum: ["BASIC", "PREMIUM", "VIP"],
+  },
+
   status: {
     type: String,
     enum: ["PENDING", "SUCCESS", "FAILED"],
     default: "PENDING",
   },
-  createdAt: { type: Date, default: Date.now },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
 
 export default mongoose.model("Payment", paymentSchema);
