04f85c3c05488bb872cd7e9520955fbc8e9efd64
tarun101727 <gaddamtarun157@gmail.com>
2026-06-09 22:50:57 +0530

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,41 +1,60 @@
 import axios from "axios";
 import Payment from "../models/Payment.js";
 
-export const createOrder = async(req,res)=>{
+export const createOrder = async (req, res) => {
 
-const {planName,amount} = req.body;
+  try {
 
-const orderId =
-"WS_" + Date.now();
+    const { planName, amount } = req.body;
 
-const response = await axios.post(
-"https://api.cashfree.com/pg/orders",
-{
-  order_amount: amount,
-  order_currency: "INR",
-  order_id: orderId,
+    const orderId = "WS_" + Date.now();
 
-  customer_details: {
-    customer_id: req.user.id,
-    customer_email: req.user.email,
-    customer_phone: "9999999999"
+    const response = await axios.post(
+      "https://api.cashfree.com/pg/orders",
+      {
+        order_amount: amount,
+        order_currency: "INR",
+        order_id: orderId,
+
+        customer_details: {
+          customer_id: req.user.id,
+          customer_email: req.user.email,
+          customer_phone: "9999999999"
+        }
+      },
+      {
+        headers: {
+          "x-client-id": process.env.CASHFREE_APP_ID,
+          "x-client-secret": process.env.CASHFREE_SECRET_KEY,
+          "x-api-version": "2023-08-01",
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
+    console.log(
+      "CASHFREE RESPONSE:",
+      response.data
+    );
+
+    await Payment.create({
+      userId: req.user.id,
+      orderId,
+      amount
+    });
+
+    res.json(response.data);
+
+  } catch (error) {
+
+    console.log(
+      "CASHFREE ERROR:",
+      error.response?.data || error.message
+    );
+
+    res.status(500).json({
+      message: "Cashfree Error",
+      error: error.response?.data || error.message
+    });
   }
-},
-{
-  headers: {
-    "x-client-id": process.env.CASHFREE_APP_ID,
-    "x-client-secret": process.env.CASHFREE_SECRET_KEY,
-    "x-api-version": "2023-08-01",
-    "Content-Type": "application/json"
-  }
-}
-);
-
-await Payment.create({
-userId:req.user.id,
-orderId,
-amount
-});
-
-res.json(response.data);
 };
