45efdb1dd59bc45cfc50b76b2da568223697a08c
tarun101727 <gaddamtarun157@gmail.com>
2026-06-09 22:28:55 +0530

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -8,26 +8,26 @@ const {planName,amount} = req.body;
 const orderId =
 "WS_" + Date.now();
 
-const response =
-await axios.post(
-"https://sandbox.cashfree.com/pg/orders",
+const response = await axios.post(
+"https://api.cashfree.com/pg/orders",
 {
-order_amount:amount,
-order_currency:"INR",
-order_id:orderId,
-customer_details:{
-customer_id:req.user.id,
-customer_email:req.user.email
-}
+  order_amount: amount,
+  order_currency: "INR",
+  order_id: orderId,
+
+  customer_details: {
+    customer_id: req.user.id,
+    customer_email: req.user.email,
+    customer_phone: "9999999999"
+  }
 },
 {
-headers:{
-"x-client-id":
-process.env.CASHFREE_APP_ID,
-"x-client-secret":
-process.env.CASHFREE_SECRET_KEY,
-"x-api-version":"2023-08-01"
-}
+  headers: {
+    "x-client-id": process.env.CASHFREE_APP_ID,
+    "x-client-secret": process.env.CASHFREE_SECRET_KEY,
+    "x-api-version": "2023-08-01",
+    "Content-Type": "application/json"
+  }
 }
 );
 
