70b3f0158df9e5ffd84ed2fb51cb515f6633e68e
tarun101727 <gaddamtarun157@gmail.com>
2026-06-10 18:11:40 +0530

    Create subscriptionPlans.js

---
+++ utils/subscriptionPlans.js
@@ -0,0 +1,16 @@
+export const SUBSCRIPTION_PLANS = {
+  BASIC: {
+    amount: 99,
+    days: 30,
+  },
+
+  PREMIUM: {
+    amount: 299,
+    days: 30,
+  },
+
+  VIP: {
+    amount: 599,
+    days: 30,
+  },
+};
